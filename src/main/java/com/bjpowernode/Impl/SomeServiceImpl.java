da46c9d5ca80792da7f66d0e8afb920b0c10a379
cwn <374136207@qq.com>
2021-05-27 10:42:11 +0800

    初始上传

---
+++ src/main/java/com/bjpowernode/Impl/SomeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.Impl;
+
+import com.bjpowernode.SomeService;
+
+public class SomeServiceImpl implements SomeService {
+    public SomeServiceImpl(){
+        System.out.println("SomeServiceImpl构造方法执行！");
+    }
+    @Override
+    public void doSome() {
+        System.out.println("执行了doSome方法");
+    }
+}
---
+++ src/main/java/com/bjpowernode/SomeService.java
@@ -0,0 +1,5 @@
+package com.bjpowernode;
+
+public interface SomeService {
+    void doSome();
+}
